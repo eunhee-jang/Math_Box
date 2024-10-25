4e5b9d6d333d6ae049ecc26accf070fa68616d69
eunhee-jang <eunhit_j@naver.com>
2024-10-25 15:45:09 +0900

    add say_hello function

--- calculator.py
+++ calculator.py
@@ -1,11 +1,13 @@
 # 기본 계산기
 
 def add(a, b):
-		return a+b
+	return a+b
 
 def subtract(a, b):
-		return a-b
+	return a-b
 
 def divide_main(a, b):
     return a / b
 
+def say_hello():
+	print("hello")
